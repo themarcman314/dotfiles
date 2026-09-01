bffb8939e22a65b14147b977f0841d92d7c26aaf
Marc Robison <marc@marcrobison.com>
2026-09-01 10:10:27 +0200

    feat: hypr swallow and waybar remove waybar applet

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -209,9 +209,9 @@ hl.config({
 	misc = {
 		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
-		enable_swallow = false,
+		enable_swallow = true,
 
-		--swallow_regex = "^(kitty)$",
+		swallow_regex = "^(kitty)$",
 	},
 })
 
